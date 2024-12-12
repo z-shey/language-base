d044f1609f0fde5b2a29ca18d1fe089c771ef51c
z-shey <z_shey@163.com>
2024-12-12 11:34:38 +0800

    Some examples of fmt

---
+++ golang/golang-demo01/fmt-Print/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("---------")
+	fmt.Print("=========")
+	fmt.Print("---------")
+}
---
+++ golang/golang-demo01/fmt-Printf/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, World!, %d\n", 10)
+	fmt.Printf("Hello, %s, %d\n", "World", 10)
+}
